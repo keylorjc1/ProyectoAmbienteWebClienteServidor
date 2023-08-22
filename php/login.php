250ff1927d9d7afea9e70ac3a8132a0b49abcd2b
Jonathan Anchia <jonhanchia2@gmail.com>
2023-08-22 17:36:53 -0600

    Updates Signup and login

--- php/login.php
+++ php/login.php
@@ -27,6 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if ($user_id !== null && password_verify($password, $stored_password)) {
     $_SESSION["username"] = $username;
     $_SESSION["usuario_id"] = $user_id;
+    $_SESSION["nombre_cliente"] = obtenerNombreCliente($user_id); // Función para obtener el nombre del cliente
     header("Location: ../index.html");  // Redirigir a la página principal
     session_destroy();
     exit();
@@ -36,5 +37,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
   $connection->close();
   
+}
+
+function obtenerNombreCliente($clienteID) {
+  $connection = new mysqli("localhost", "root", "", "wapicr");
+
+  if ($connection->connect_error) {
+      die("Error de conexión: " . $connection->connect_error);
+  }
+
+  $query = "SELECT Nombre FROM cliente WHERE IDCliente = ?";
+  $stmt = $connection->prepare($query);
+  $stmt->bind_param("i", $clienteID);
+  $stmt->execute();
+  $stmt->bind_result($nombre);
+  $stmt->fetch();
+  $stmt->close();
+
+  $connection->close();
+  return $nombre;
 }
 ?>
